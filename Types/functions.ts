bd661fdc2eaafeccc0f1e68b9c7202e3faef34be
Joo Ka <mr.jooka@gmail.com>
2023-04-24 06:40:14 +0900

    18.기본 파라미터

--- Types/functions.ts
+++ Types/functions.ts
@@ -4,12 +4,18 @@ function square(n: number) {
 
 square(2);
 
-function greet(name: string) {
-  return `Hello ${name.toUpperCase()}`;
-}
+// function greet(name: string) {
+//   return `Hello ${name.toUpperCase()}`;
+// }
 
 greet("mike");
 
 const doSomething = (person: string, age: number, isFunny: boolean) => {};
 
-doSomething("mike");
+doSomething("mike", 19, true);
+
+function greet(name: string = "stranger") {
+  return `Hello ${name.toUpperCase()}`;
+}
+
+greet();
