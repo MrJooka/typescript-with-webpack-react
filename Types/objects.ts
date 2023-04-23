a00ecf2c4523ba1b69ef178d806aabfe56b401f2
Joo Ka <mr.jooka@gmail.com>
2023-04-24 07:22:35 +0900

    27. type Alias 타입 별칭

--- Types/objects.ts
+++ Types/objects.ts
@@ -1,25 +1,40 @@
-const dog = {
-  name: "Bingo",
-  age: 5,
-  breed: "Dalmatian",
-};
+// const dog = {
+//   name: "Bingo",
+//   age: 5,
+//   breed: "Dalmatian",
+// };
 
-function printName(animal: { name: string; age: number; breed: string }): void {
-  console.log(animal.name);
-}
+// function printName(animal: { name: string; age: number; breed: string }): void {
+//   console.log(animal.name);
+// }
 
-printName(dog);
+// printName(dog);
 
-let coordinate: { x: number; y: number } = { x: 10, y: 20 };
+// let coordinate: { x: number; y: number } = { x: 10, y: 20 };
 
-function printCoordinate1(): { x: number; y: number } {
+// function printCoordinate1(): { x: number; y: number } {
+//   return { y: 12, x: 14 };
+// }
+
+// function printCoordinate2(): typeof coordinate {
+//   return { y: 12, x: 14 };
+// }
+
+// printName({ name: "Bingo", age: 5, breed: "Dalmatian", isFemale: false });
+// const cat = { name: "Bingo", age: 5, breed: "Dalmatian", isFemale: false };
+// printName(cat);
+
+type Point = { x: number; y: number };
+
+let coordinate: Point = { x: 10, y: 20 };
+
+function printCoordinate1(): Point {
   return { y: 12, x: 14 };
 }
 
-function printCoordinate2(): typeof coordinate {
-  return { y: 12, x: 14 };
+function doublePoint(p: Point): Point {
+  return { x: p.x * 2, y: p.y * 2 };
 }
 
-printName({ name: "Bingo", age: 5, breed: "Dalmatian", isFemale: false });
-const cat = { name: "Bingo", age: 5, breed: "Dalmatian", isFemale: false };
-printName(cat);
+type myNumber = number;
+let age: myNumber = 10;
